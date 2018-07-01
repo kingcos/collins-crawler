9b61af6d7f5a787335fe4a003232bce9a8f2fe41
kingcos <2821836721v@gmail.com>
2018-07-01 20:21:42 +0800

    Save to models

--- CollinsCrawler.py
+++ CollinsCrawler.py
@@ -3,6 +3,10 @@
 import re
 from urllib import urlopen
 from bs4 import BeautifulSoup
+from Model.Word import Word
+from Model.Type import Type
+from Model.Meaning import Meaning
+from Model.Example import Example
 
 sample_path_vegetable = 'sample/vegetable-youdao.html'
 sample_path_make = 'sample/make-youdao.html'
@@ -28,45 +32,53 @@ def result(word, html):
     collins_content = beautiful_soup.select_one('div.collinsToggle')
     # Construct models
     # Word
+    word = Word()
+
     word_h4 = collins_content.find('h4')
-    word_name = word_h4.find(name='span', attrs={'class': 'title'}).get_text()
-    word_phonetic = word_h4.find(name='em', attrs={'class': 'phonetic'}).get_text()
-    word_star = word_h4.find(name='span', attrs={'class': 'star'}).attrs['class'][1]
-    word_ranks = map(lambda s: s.get_text().split(' '),
-                     word_h4.find_all(name='span', attrs={'class': 'rank'}))
-    word_additional = map(lambda s: s.strip(),
+    word.name = word_h4.find(name='span', attrs={'class': 'title'}).get_text()
+    word.phonetic = word_h4.find(name='em', attrs={'class': 'phonetic'}).get_text()
+    word.frequency = word_h4.find(name='span', attrs={'class': 'star'}).attrs['class'][1]
+    word.ranks = word_h4.find(name='span', attrs={'class': 'rank'}).get_text().split(' ')
+    word.additional = map(lambda s: s.strip(),
                           re.sub('\\(|\\)', '',
                                  word_h4.find(name='span', attrs={'class': 'additional'}).get_text())
                           .split(','))
 
     # print(word_name, word_phonetic, word_star, word_ranks, word_additional)
 
+    # Type
     word_types = collins_content.find_all(name='div', attrs={'class': 'wt-container'})
     for word_type in word_types:
-        type_name = ''
-        if len(word_types) > 1:
-            type_name = word_type.find(name='div', attrs={'class': 'trans-tip'}).find('span').get_text()
+        type = Type()
+        type.name = word_type.find(name='div', attrs={'class': 'trans-tip'}).find('span').get_text() if len(word_types) > 1 else None
 
         # Meaning
         type_meanings = word_type.find_all(name='div', attrs={'class': 'collinsMajorTrans'})
         for type_meaning in type_meanings:
-            meaning_additional = map(lambda s: s.get('title') if s.get('title') is not None else s.get_text(),
+            meaning = Meaning()
+
+            meaning.additional = map(lambda s: s.get('title') if s.get('title') is not None else s.get_text(),
                                      type_meaning.find_all(name='span', attrs={'class': 'additional'}))
             map(lambda s: s.clear(),
                 type_meaning.find('p').find_all(name='span', attrs={'class': 'additional'}))
-            meaning_description = type_meaning.find('p').get_text().strip()
+
+            meaning.description = type_meaning.find('p').get_text().strip()
 
             # Example
             meaning_examples = collins_content.find_all(name='div', attrs={'class': 'exampleLists'})
             for meaning_example in meaning_examples:
+                example = Example()
                 sentences = meaning_example.find_all('p')
-                example_english = sentences[0].get_text().strip()
-                example_chinese = sentences[1].get_text().strip()
-    return ''
+                example.english = sentences[0].get_text().strip()
+                example.chinese = sentences[1].get_text().strip()
+                meaning.examples.append(example)
+            type.meanings.append(meaning)
+        word.types.append(type)
+    return word
 
 
 # Look up the word
 def look_up(word):
-    # html = read(sample_path_make)
+    html = read(sample_path_make)
     html = fetch(word)
     return result(word, html)
