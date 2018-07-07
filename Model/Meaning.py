916dd8b41957816cf6660440e00db2c0260d9c6c
kingcos <2821836721v@gmail.com>
2018-07-07 20:11:02 +0800

    Refact Meaning

--- Model/Meaning.py
+++ Model/Meaning.py
@@ -1,9 +1,15 @@
 # -*- coding:utf-8 -*-
 
+from functools import reduce
+
 
 class Meaning:
     id = ''
-    typeID = ''
+    type_id = ''
     description = ''
-    additional = ''
+    additional = []
     examples = []
+
+    def desc(self):
+        additional = reduce(lambda x, y: x + ' ' + y, self.additional) if len(self.additional) > 0 else ''
+        return '(\'' + self.type_id + '\', \'' + self.description + '\', \'' + additional + '\')'
