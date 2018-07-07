9d0bb87e0c2bb55adcc0f535abe451ab6c507f37
kingcos <2821836721v@gmail.com>
2018-07-07 21:24:56 +0800

    Removed useless comments

--- main.py
+++ main.py
@@ -8,14 +8,8 @@ from SQLiter import SQLiter
 if __name__ == '__main__':
     word = 'function'
     result = CollinsCrawler.look_up(word)
-    # # print(result)
-    # for t in result.types:
-    #     for m in t.meanings:
-    #         for e in m.examples:
-    #             print(e.english)
-    #             print(e.chinese)
 
-    # with Keeper('localhost', 'kingcos', '9512', 'WordList') as keeper:
+    # with Keeper('localhost', 'kingcos', '1234', 'WordList') as keeper:
     #     keeper.create_tables()
     with SQLiter('WordList.db') as liter:
         liter.create_tables()
